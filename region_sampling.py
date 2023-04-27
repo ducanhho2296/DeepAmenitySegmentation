988f45bdbe0c397c5ea4ef92f20c937a741aa7dd
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-04-27 17:01:15 +0200

    update

--- region_sampling.py
+++ region_sampling.py
@@ -70,11 +70,11 @@ class Gridgenerating():
             max_y = max(point3.y, point4.y)
 
             square_bbox = (min_x, min_y, max_x, max_y)
-            self.bbox_df = gpd.GeoDataFrame(geometry=[self.create_bbox_poly(square_bbox)]) #=> can be used to visualize location of bbox inside city
+            bbox_df = gpd.GeoDataFrame(geometry=[self.create_bbox_poly(square_bbox)]) #=> can be used to visualize location of bbox inside city
             
             self.bbox_df.crs = self.city_df.crs
             #create building dataframe storing buildings inside bbox
             buildings_within = gpd.sjoin(self.city_df, self.bbox_df, predicate="intersects") 
-            return buildings_within
+            return buildings_within, bbox_df
         else:
             return None
