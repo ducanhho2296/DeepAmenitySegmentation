c05d1942649b837e8eb440318d874508c8929e07
ducanhho2296 <ducanh.ho2296@gmail.com>
2023-05-08 09:40:39 +0200

    update

--- main.py
+++ main.py
@@ -50,46 +50,54 @@ grid_points = grid_generating.grid_points_in_bbox(bbox=city_bbox)
 
 img_size = 512
 count = 0
-total_iterations = len(grid_points)-1
-for i in range(0, total_iterations):
-        buildings_within_bbox = None
-        bbox_visual = None
-        img = None
-        buildings_within_bbox, bbox_visual = grid_generating.capture_square_bbox(point_position=i,grid_points=grid_points) 
-        if i % (total_iterations // 10) == 0:
-                progress = (i / total_iterations) * 100
-                print(f"Processing figures and extracting label: {progress:.2f}%")
+total_iterations = len(grid_points) - 1
 
-        #capture only bboxs which have buildings inside
-        if len(buildings_within_bbox) !=0 : 
-                count += 1
+for i in range(total_iterations):
+    buildings_within_bbox = None
+    bbox_visual = None
+    img = None
 
-                fig, ax = plt.subplots(figsize=(10,10))
-                fig.patch.set_facecolor('black')
-                #plotting buildings_within_bbox for generating bbox's figures
-                buildings_within_bbox.plot(ax=ax, edgecolor='white', facecolor='white')
-                plt.axis('off')
-                #save figures in buffer memory
-                img_buffer = BytesIO()
-                plt.savefig(img_buffer, dpi=100, format="jpg", bbox_inches='tight')
-                img_buffer.seek(0)
-                img = Image.open(img_buffer)
-                img = np.array(img)        
-                #generate label for img
-                # label_image(buildings_within_bbox, img, write_path=label_path.format(i))
+    buildings_within_bbox, bbox_visual = grid_generating.capture_square_bbox(point_position=i, grid_points=grid_points) 
 
-                label_data, label_profile = label_image(buildings_within_bbox, img, i)
-                tif_buffer = BytesIO()
-                with rasterio.open(tif_buffer, 'w', **label_profile) as label_dst:
-                        label_dst.write(label_data)
-                tif_buffer.seek(0)
-                #process padding image from rectangle to fixed square size 
-                padded_img = pad_image_to_square(img, img_size)
-                cv2.imwrite(padded_img_path.format(count), padded_img)
-                #process padding label
-                pad_tif_to_square(tif_buffer, padded_label_path.format(count), img_size)
-                plt.close()
-                # img_buffer.close()
-                # tif_buffer.close()
+    if i % (total_iterations // 10) == 0:
+        progress = (i / total_iterations) * 100
+        print(f"Processing figures and extracting label: {progress:.2f}%")
+
+    # Capture only bounding boxes that have buildings inside
+    if len(buildings_within_bbox) != 0:
+        count += 1
+        
+        fig, ax = plt.subplots(figsize=(10, 10))
+        fig.patch.set_facecolor('black')
+        
+        # Plot buildings_within_bbox for generating bounding box figures
+        buildings_within_bbox.plot(ax=ax, edgecolor='white', facecolor='white')
+        plt.axis('off')
+        
+        # Save figures in buffer memory
+        img_buffer = BytesIO()
+        plt.savefig(img_buffer, dpi=100, format="jpg", bbox_inches='tight')
+        img_buffer.seek(0)
+        img = Image.open(img_buffer)
+        img = np.array(img)  
+        
+        # Generate label for img
+        label_data, label_profile = label_image(buildings_within_bbox, img, i)
+        tif_buffer = BytesIO()
+        
+        with rasterio.open(tif_buffer, 'w', **label_profile) as label_dst:
+            label_dst.write(label_data)
+        tif_buffer.seek(0)
+        
+        # Process padding image from rectangle to fixed square size 
+        padded_img = pad_image_to_square(img, img_size)
+        cv2.imwrite(padded_img_path.format(count), padded_img)
+        
+        # Process padding label
+        pad_tif_to_square(tif_buffer, padded_label_path.format(count), img_size)
+        plt.close()
+        
+        img_buffer.close()
+        tif_buffer.close()
+
 print("Adding padding to bounding box and processing images successfully")
-
