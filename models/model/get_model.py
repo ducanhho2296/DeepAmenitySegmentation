1110646a7b71cbef0da0fc7774c2d81c5e42eab4
ducanhho2296 <ducanh.ho2296@gmail.com>
2023-05-08 22:28:48 +0200

    big upgrade to change num_classes, fix bugs

---
+++ models/model/get_model.py
@@ -0,0 +1,19 @@
+import segmentation_models_pytorch as smp
+
+def get_unet_model(device, in_channels, num_classes):
+    model = smp.Unet(
+        encoder_name="efficientnet-b0",
+        encoder_weights="imagenet",
+        in_channels=in_channels,
+        classes=num_classes
+    ).to(device)
+    return model
+
+def get_deeplabv3plus_model(device, in_channels, num_classes):
+    model = smp.DeepLabV3Plus(
+        encoder_name="efficientnet-b7",
+        encoder_weights="imagenet",
+        in_channels=in_channels,
+        classes=num_classes
+    ).to(device)
+    return model
