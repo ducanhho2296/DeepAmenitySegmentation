24b47d5a5ac9a39763822e1e9984edab13431aaa
ducanhho2296 <ducanh.ho2296@gmail.com>
2023-05-12 14:36:33 +0200

    adding function for  generating testset

--- datasets/generate_dataset.py
+++ datasets/generate_dataset.py
@@ -47,8 +47,8 @@ padded_img_path = os.path.join('..', root_path, padded_img_dir, 'image{}.jpg')
 padded_label_path = os.path.join('..', root_path, padded_label_dir, 'label{}.tif')
 
 #test_path
-test_image_path = os.path.join(root_path, test_image_dir)
-test_label_path = os.path.join(root_path, test_label_dir)
+test_image_path = os.path.join('..',root_path, test_image_dir, 'image{}.jpg')
+test_label_path = os.path.join('..',root_path, test_label_dir, 'label{}.tif')
 
 city_data = CityData(city_name)
 buildings, city_bbox = city_data.get_data()
@@ -115,10 +115,9 @@ def grid_sampling():
             img_buffer.close()
             tif_buffer.close()
 
-    print("Generating square bbox images with Grid-sampling technique successfully")
+    print("Generating square bbox images with Grid-sampling successfully")
     print("Adding padding to bounding box and processing images successfully")
     print("-----------------------------------------------------------------------------------------------------")
-    input("Press enter to continue generating new type of square bbox images with all amenity points inside city")
 
     #overwritting number of classes into config.ini file for training process
     # Load the config file
@@ -141,7 +140,7 @@ def extract_point(geometry):
         largest_polygon = max(geometry.geoms, key=lambda x: x.area)
         return np.array(largest_polygon.centroid.coords[0])
 
-def random_generate():
+def random_generate(save=True):
     amenity_points = []
     # extract all POINT type coordinates
     retail = buildings[buildings.function == "retail"]
@@ -185,33 +184,96 @@ def random_generate():
     exploded = buildings.explode(index_parts=True)
 
     dist = int(config['city']['dist'])
-    for j in amenity_points:
-        # raster = np.zeros(shape=(size,size))  # A[i][j] = "(long, latt)"
-        center_point = (j[1], j[0]) #center_point = (latitude, longtitude)
-        bbox = ox.utils_geo.bbox_from_point(center_point, dist=dist)
+    if save:
+        for j in amenity_points:
+            # raster = np.zeros(shape=(size,size))  # A[i][j] = "(long, latt)"
+            center_point = (j[1], j[0]) #center_point = (latitude, longtitude)
+            bbox = ox.utils_geo.bbox_from_point(center_point, dist=dist)
 
-        bbox_polygon = Polygon([(bbox[3],bbox[1]), (bbox[2], bbox[1]),(bbox[2], bbox[0]), (bbox[3], bbox[0])])
+            bbox_polygon = Polygon([(bbox[3],bbox[1]), (bbox[2], bbox[1]),(bbox[2], bbox[0]), (bbox[3], bbox[0])])
+            #checking if polygons inside the bbox
+            check_polygon = exploded.geometry.within(bbox_polygon)
+
+            #return all rows, which have polygons inside bbox
+            polygon_inbbox = check_polygon[check_polygon.values == True]
+
+            #using isin() to extract subtable contains only polygon_inbbox.index
+            indexs = polygon_inbbox.index
+            condition = exploded['geometry'].index.isin(indexs)
+            buildings_inbbox = exploded[condition]
+
+            #mapping amenities inside bbox into the new axis
+            if len(buildings_inbbox.geometry) != 0: #perform labelling only when there is building inside bbox
+            #plotting time
+                fig1, ax1 = plt.subplots(figsize=(10,10))
+                ax1.set_aspect(1)
+                fig1.patch.set_facecolor('black')
+                plt.axis('off')
+                buildings_inbbox.plot(ax=ax1, edgecolor='white', facecolor='white')
+                global count
+                count += 1
+                
+                img_buffer = BytesIO()
+                plt.savefig(img_buffer, dpi=100, format="jpg", bbox_inches='tight')
+                img_buffer.seek(0)
+                img = Image.open(img_buffer)
+                img = np.array(img)  
+                
+                # Generate label for img
+                label_data, label_profile, num_classes = label_image(buildings_inbbox, img, j)
+                tif_buffer = BytesIO()
+                
+                with rasterio.open(tif_buffer, 'w', **label_profile) as label_dst:
+                    label_dst.write(label_data)
+                tif_buffer.seek(0)
+                
+                # Process padding image from rectangle to fixed square size 
+                padded_img = pad_image_to_square(img, img_size)
+                cv2.imwrite(padded_img_path.format(count), padded_img)
+                
+                # Process padding label
+                pad_tif_to_square(tif_buffer, padded_label_path.format(count), img_size)
+
+                plt.close()
+                
+                img_buffer.close()
+                tif_buffer.close()
+        
+        print("Adding padding to bounding box and processing images successfully")
+
+    input("press Enter to create test sets from amenity points")
+    test_dist = int(config['city']['test_dist'])
+    c = 0
+    for j in range(round(len(amenity_points)/3)):
+        point = amenity_points[j]
+        # raster = np.zeros(shape=(size,size))  # A[i][j] = "(long, latt)"
+        center_point = (point[1], point[0]) #center_point = (latitude, longtitude)
+        bbox_test = ox.utils_geo.bbox_from_point(center_point, dist=test_dist)
+
+        bbox_polygon_test = Polygon([(bbox_test[3],bbox_test[1]), 
+                                     (bbox_test[2], bbox_test[1]),
+                                     (bbox_test[2], bbox_test[0]), 
+                                     (bbox_test[3], bbox_test[0])])
         #checking if polygons inside the bbox
-        check_polygon = exploded.geometry.within(bbox_polygon)
+        check_polygon_test = exploded.geometry.within(bbox_polygon_test)
 
         #return all rows, which have polygons inside bbox
-        polygon_inbbox = check_polygon[check_polygon.values == True]
+        polygon_inbbox_test = check_polygon_test[check_polygon_test.values == True]
 
         #using isin() to extract subtable contains only polygon_inbbox.index
-        indexs = polygon_inbbox.index
-        condition = exploded['geometry'].index.isin(indexs)
-        buildings_inbbox = exploded[condition]
+        indexs_test = polygon_inbbox_test.index
+        condition_test = exploded['geometry'].index.isin(indexs_test)
+        buildings_inbbox_test = exploded[condition_test]
 
         #mapping amenities inside bbox into the new axis
-        if len(buildings_inbbox.geometry) != 0: #perform labelling only when there is building inside bbox
+        if len(buildings_inbbox_test.geometry) != 0: #perform labelling only when there is building inside bbox
         #plotting time
-            fig1, ax1 = plt.subplots(figsize=(10,10))
-            ax1.set_aspect(1)
-            fig1.patch.set_facecolor('black')
+            fig2, ax2 = plt.subplots(figsize=(10,10))
+            ax2.set_aspect(1)
+            fig2.patch.set_facecolor('black')
             plt.axis('off')
-            buildings_inbbox.plot(ax=ax1, edgecolor='white', facecolor='white')
-            global count
-            count += 1
+            buildings_inbbox_test.plot(ax=ax2, edgecolor='white', facecolor='white')
+            c += 1
             
             img_buffer = BytesIO()
             plt.savefig(img_buffer, dpi=100, format="jpg", bbox_inches='tight')
@@ -220,7 +282,7 @@ def random_generate():
             img = np.array(img)  
             
             # Generate label for img
-            label_data, label_profile, num_classes = label_image(buildings_inbbox, img, j)
+            label_data, label_profile, num_classes = label_image(buildings_inbbox_test, img, j)
             tif_buffer = BytesIO()
             
             with rasterio.open(tif_buffer, 'w', **label_profile) as label_dst:
@@ -229,23 +291,22 @@ def random_generate():
             
             # Process padding image from rectangle to fixed square size 
             padded_img = pad_image_to_square(img, img_size)
-            cv2.imwrite(padded_img_path.format(count), padded_img)
+            cv2.imwrite(test_image_path.format(c), padded_img)
             
             # Process padding label
-            pad_tif_to_square(tif_buffer, padded_label_path.format(count), img_size)
+            pad_tif_to_square(tif_buffer, test_label_path.format(c), img_size)
 
             plt.close()
             
             img_buffer.close()
             tif_buffer.close()
-    
-    print("Adding padding to bounding box and processing images successfully")
-
-    input("press Enter to create test sets ")
+    print("Generating test dataset successfully with {} images.".format(c))
 
 
 
 
 if __name__ == "__main__":
     grid_sampling()
-    random_generate()
+    input("Press enter to continue generating datasets with amenity sampling method")
+    random_generate(save=True) #change save=False to extract only test set 
+                                #without generating training set of amenity sampling method.
