7bdc8a23ba7a5c40660787e42f6ea52dfd83bc9e
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-08-14 06:34:08 +0200

    Update region_sampling.py

--- utils/region_sampling.py
+++ utils/region_sampling.py
@@ -21,7 +21,6 @@ class Gridgenerating():
                 self.grid_points.append(Point(x, y))
                 
         return self.grid_points
-    #     #longitudes, latitudes = zip(*points_list) #just for ploting
     #     return points_list
     
     def create_bbox_poly(self,square_bbox):
