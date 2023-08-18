8273b2a2200c67e2baa64165aace86c7a4a407e4
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-08-18 09:59:53 +0200

    Update convert.py

--- utils/convert.py
+++ utils/convert.py
@@ -2,3 +2,4 @@ import rasterio
 import numpy as np
 
 def convert_label_to_coordinates_with_class_names(predicted_label_image, label_profile, class_mapping):
+    # Open the label image using rasterio
