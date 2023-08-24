0cdf8d4d123d2bb413bfaf7d4bfd469000690595
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-08-24 08:59:50 +0200

    Update convert.py

--- utils/convert.py
+++ utils/convert.py
@@ -19,3 +19,4 @@ def convert_label_to_coordinates_with_class_names(predicted_label_image, label_p
     
     return lon_values, lat_values, predicted_class_names
 
+# Call the function to convert the predicted label image to coordinates with class names
