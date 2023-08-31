831f782a28e3536ecda08d27cebc290ca4bc6a13
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-08-31 08:59:51 +0200

    Update image_processing_test.py

--- pytest/image_processing_test.py
+++ pytest/image_processing_test.py
@@ -8,3 +8,9 @@ import pytest
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
 
+from utils.image_processing import pad_image_to_square, pad_tif_to_square
+import configparser
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
