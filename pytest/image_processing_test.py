781a9f3fca22f5d6e7c7a3f16ebb54d8892b0c58
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-09-21 10:47:44 +0200

    Update image_processing_test.py

--- pytest/image_processing_test.py
+++ pytest/image_processing_test.py
@@ -24,6 +24,7 @@ def test_load_image():
     # Attempt to load the test image
     try:
         test_image = cv2.imread(test_image_path)
+        pytest.fail(f"Failed to load the image: {str(e)}")
 
 def test_pad_image_to_square():
     test_image = cv2.imread(test_image_path)
