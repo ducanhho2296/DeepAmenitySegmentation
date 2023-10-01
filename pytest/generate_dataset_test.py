15b8d98e929f20d66913b16769b79f9d755e7d7d
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-10-01 14:37:22 +0700

    Update generate_dataset_test.py

--- pytest/generate_dataset_test.py
+++ pytest/generate_dataset_test.py
@@ -6,3 +6,4 @@ import configparser
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
 
+from datasets.generate_dataset import grid_sampling, stratified_sampling
