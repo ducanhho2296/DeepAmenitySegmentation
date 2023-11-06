29a36c27e65be0349f86876c8e106f4299befceb
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-11-06 10:58:09 +0700

    Update region_sampling.py

--- utils/region_sampling.py
+++ utils/region_sampling.py
@@ -38,6 +38,7 @@ class Gridgenerating():
     
     def capture_square_bbox(self, point_position, grid_points=None):
         """
+        draw out the bbox in map
         if grid_points is None:
             grid_points = self.grid_points
 
