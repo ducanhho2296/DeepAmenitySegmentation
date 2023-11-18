aeee8f50c8f2985c644ec87fd1d8eee25b475d25
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-11-18 08:54:51 +0700

    Update image_processing.py

--- utils/image_processing.py
+++ utils/image_processing.py
@@ -32,6 +32,7 @@ def pad_image_to_square(img, size=448):
 #         profile = src.profile
 #         profile.update(width=padded_img.shape[1], height=padded_img.shape[0], transform=transform)
 #     with rasterio.open(output_file, 'w', **profile) as dst:
+#         padded_img = np.moveaxis(padded_img, -1, 0)
 # def pad_tif_to_square(input_file, output_file):
 #     with rasterio.open(input_file) as src:
 #         img = src.read()
