ea2a87a3f75b24285270e53a204f7132a9cbabe8
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-12-11 08:18:06 +0100

    Update label_image.py

--- utils/label_image.py
+++ utils/label_image.py
@@ -53,3 +53,4 @@ def label_image(building_df, image, i=1):
 #         profile = src.profile
 #         profile.update(width=padded_img.shape[1], height=padded_img.shape[0], transform=transform)
 #     with rasterio.open(output_file, 'w', **profile) as dst:
+#         padded_img = np.moveaxis(padded_img, -1, 0)
