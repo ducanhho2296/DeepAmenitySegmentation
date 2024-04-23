b900443de3a8ef780043ce9ac72fa3e86c40bba9
Duc Anh Ho <ducanh.ho2296@gmail.com>
2024-04-23 11:40:02 +0200

    Automated synchronization

--- sync_script.sh
+++ sync_script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# execute git pull to triggle .git/hooks/post-merge
+git pull
+
 # Example: Commit changes and push to remote (demo_ducanh)
 git add .
 git commit -m "Automated synchronization"
